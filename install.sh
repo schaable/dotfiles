2604c2078cc27cabf46d4f4955212d486e73298f
Luis Schaab <schaable@gmail.com>
2025-10-08 23:47:17 +0100

    update

--- install.sh
+++ install.sh
@@ -1,10 +1,31 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-set -e
-set -x
+# exit on error (-e), 
+# error on unset vars (-u), 
+# fail if any command in a pipeline fails (pipefail),
+# and trace commands as they run (-x)
+set -euo pipefail -x
+
+clone_or_update() {
+  local repo="$1" dest="$2"
+  if [ -d "$dest/.git" ]; then
+    git -C "$dest" pull --ff-only --quiet || true
+  else
+    git clone --depth=1 "$repo" "$dest" >/dev/null 2>&1 || true
+  fi
+}
+
+ZSH_CUSTOM="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"
+
+# Install Powerlevel10k theme
+clone_or_update https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+
+# Install zsh plugins
+clone_or_update https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+clone_or_update https://github.com/zsh-users/zsh-completions $ZSH_CUSTOM/plugins/zsh-completions
 
 # Copy dotfiles
-cp ~/dotfiles/.zshrc ~/.zshrc
 cp ~/dotfiles/.p10k.zsh ~/.p10k.zsh
+cp ~/dotfiles/.zshrc ~/.zshrc
 
 echo "Dotfiles installed!"
